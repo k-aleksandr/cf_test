82765d28229367d132180f16a38bc0ee182d2866
Oleksandr Koshedran <oleksandr_koshedran@epam.com>
2020-11-13 12:41:28 +0200

    commit13

---
+++ devo-engine/ambda_function.py
@@ -0,0 +1,13 @@
+import boto3
+import urllib.request, urllib.parse, urllib.error
+import json
+import re
+import logging
+import os
+import copy
+import json
+import re
+import zlib
+from collections import OrderedDict
+from devo_engine import *
+
